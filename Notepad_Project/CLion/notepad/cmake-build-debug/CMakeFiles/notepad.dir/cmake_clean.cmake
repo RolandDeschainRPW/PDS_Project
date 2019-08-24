ab05a882625d80b04edee94c79af28aa1f68cd83
RolandDeschainRPW <raffaele.plmr@gmail.com>
2019-08-24 17:32:03 +0200

    Adding CLion project for notepad

---
+++ Notepad_Project/CLion/notepad/cmake-build-debug/CMakeFiles/notepad.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "notepad_autogen"
+  "CMakeFiles/notepad_autogen.dir/AutogenOldSettings.txt"
+  "notepad_autogen/EWIEGA46WW/qrc_notepad.cpp"
+  "CMakeFiles/notepad.dir/notepad_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/notepad.dir/main.cpp.obj"
+  "CMakeFiles/notepad.dir/notepad.cpp.obj"
+  "CMakeFiles/notepad.dir/notepad_autogen/EWIEGA46WW/qrc_notepad.cpp.obj"
+  "notepad.pdb"
+  "notepad.exe"
+  "notepad.exe.manifest"
+  "libnotepad.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/notepad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
